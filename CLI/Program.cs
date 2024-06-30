5164326e34c2333928b91725fcf0eeb169108464
Berat Hündürel <berat.hundurel@bilgiedu.net>
2024-06-30 23:10:50 +0300

    Adding option to add multiple paragraph or image areas for admin templates

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,31 +1,42 @@
 ﻿using CommandLine;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 
 public class Program
 {
-    public static readonly string adminSectionsPath = Path.Combine(Directory.GetCurrentDirectory(), "admin", "Views", "Shared", "Components", "Section");
-    public static readonly string adminSectionRegisterPath = Path.Combine(Directory.GetCurrentDirectory(), "admin", "Views", "Shared", "Components", "PageSections", "Default.cshtml");
-    public static readonly string clientSectionsPath = Path.Combine(Directory.GetCurrentDirectory(), "www", "Views", "Shared", "Components");
+    public static readonly string adminSectionsPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "admin", "Views", "Shared", "Components", "Section");
+    public static readonly string adminSectionRegisterPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "admin", "Views", "Shared", "Components", "PageSections", "Default.cshtml");
+    public static readonly string clientSectionsPath = Path.Combine(Directory.GetCurrentDirectory(), "..", "www", "Views", "Shared", "Components");
 
     public class Options
     {
         [Value(0, MetaName = "name", Required = true, HelpText = "The section name to create")]
         public string Name { get; set; }
+
+        [Option('r', "pr", Default = 0, HelpText = "Number of paragraphs to include in the template.")]
+        public int Paragraphs { get; set; }
+
+        [Option('i', "im", Default = 0, HelpText = "Number of images to include in the template.")]
+        public int Images { get; set; }
+
+        [Option('t', "txt", Default = 0, HelpText = "Number of textareas to include in the template.")]
+        public int Textareas { get; set; }
     }
 
     public static void Main(string[] args)
     {
         Parser.Default.ParseArguments<Options>(args)
-               .WithParsed<Options>(opts => ExecuteCommand(opts.Name));
+                 .WithParsed<Options>(opts => ExecuteCommand(opts.Name, opts.Paragraphs, opts.Images, opts.Textareas))
+                 .WithNotParsed<Options>((errs) => HandleParseError(errs));
     }
 
-    private static void ExecuteCommand(string name)
+    private static void ExecuteCommand(string name, int paragraphs, int images, int textareas)
     {
         string sectionNameWithExtension = $@"_{name}Section.cshtml";
         string sectionName = $@"_{name}Section";
-        GenerateAdminTemplates(name, sectionNameWithExtension, sectionName);
+        GenerateAdminTemplates(name, sectionNameWithExtension, sectionName, paragraphs, images, textareas);
         GenerateClientTemplates(name, sectionNameWithExtension);
     }
 
@@ -44,7 +55,7 @@ public class Program
         }
     }
 
-    private static void GenerateAdminTemplates(string name, string sectionNameWithExtension, string sectionName)
+    private static void GenerateAdminTemplates(string name, string sectionNameWithExtension, string sectionName, int paragraphs, int images, int textareass)
     {
         try
         {
@@ -69,6 +80,28 @@ public class Program
         {
             string adminSectionContent = ReadEmbeddedResource("CLI.adminSectionContent.txt");
             adminSectionContent = adminSectionContent.Replace("{sectionName}", sectionName);
+
+            if (paragraphs > 0)
+            {
+                string adminParagraphTemplate = ReadEmbeddedResource("CLI.adminParagraph.txt");
+                for (int i = 1; i < paragraphs + 1; i++)
+                {
+                    string newAdminParagraphTemplate = adminParagraphTemplate.Replace("paramText1", "paramText" + i);
+                    newAdminParagraphTemplate = newAdminParagraphTemplate.Replace("Title 1", "Title " + i);
+                    adminSectionContent += newAdminParagraphTemplate;
+                }
+            }
+
+            if (images > 0)
+            {
+                string adminImageTemplate = ReadEmbeddedResource("CLI.adminImage.txt");
+                for (int i = 1; i < images + 1; i++)
+                {
+                    string newAdminImageTemplate = adminImageTemplate.Replace("paramMediaId1", "paramMediaId" + i);
+                    newAdminImageTemplate = newAdminImageTemplate.Replace("FileId = 1", "FileId = " + i);
+                    adminSectionContent += newAdminImageTemplate;
+                }
+            }
             File.WriteAllText(Path.Combine(adminSectionsPath, sectionNameWithExtension), adminSectionContent);
         }
         catch (Exception ex)
@@ -85,5 +118,10 @@ public class Program
         {
             return reader.ReadToEnd();
         }
+    }
+
+    private static void HandleParseError(IEnumerable<Error> errs)
+    {
+        Console.WriteLine("Error parsing arguments.", errs);
     }
 }
